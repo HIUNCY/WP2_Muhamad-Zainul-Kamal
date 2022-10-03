60c1317581ffd8385a9a5f5f1934a0feb02146eb
HIUNCY <wolesajay@gmail.com>
2022-10-03 11:34:46 +0700

    Membuat Controller Contoh 1 untuk tugas Pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Muhamad Zainul Kamal. Saya tinggal di daerah Tanjung Priok, olahraga yang saya sukai adalah Cardio";
+	}
+}
